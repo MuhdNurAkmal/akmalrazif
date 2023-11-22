eacd2ae3c1446b256aa101aad3208b7675a1a3a0
Akmal Razif <akmaln02@gmail.com>
2023-11-23 01:35:22 +0800

    update homepage

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-// toggle icon navbar
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+document.addEventListener('DOMContentLoaded', function () {
+  let menuIcon = document.querySelector("#menu-icon");
+  let navbarLinks = document.querySelectorAll(".navbar a");
 
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
-};
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbarLinks.forEach(link => link.classList.toggle("active-link"));
+  };
+
+  navbarLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      navbarLinks.forEach(otherLink => otherLink.classList.remove("active-link"));
+      this.classList.add("active-link");
+    });
+  });
+});
+
+const jobTitles = ["Aspiring Data Scientist", "Computer Vision Intern"];
+  let index = 0;
+
+  function updateJobTitle() {
+    document.getElementById("jobTitle").textContent = jobTitles[index];
+    index = (index + 1) % jobTitles.length;
+  }
+
+  setInterval(updateJobTitle, 2000);
