42c6794d56368fe9730c089d244afd12ed3fc4a2
Akmal Razif <akmaln02@gmail.com>
2025-04-12 05:44:59 +0800

    update portfolio

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,28 @@
 import SectionTitle from "./SectionTitle";
 import SkillCard from "../cards/SkillCard";
-import { skills } from "../data/skillData";
+import { skillset } from "../data/skillData";
 
 const Skills = () => {
   return (
     <section className="align-element py-16">
       <div id="skills" className="pt-24">
         <SectionTitle title="Skills & Expertise" />
-        <div className="py-16 grid gap-3 grid-cols-2 md:grid-cols-6">
-          {skills.map((skill) => {
-            return <SkillCard key={skill.id} {...skill} />;
-          })}
+        <div className="py-16 px-4 grid grid-cols-1 md:grid-cols-2 md:gap-6">
+          {skillset.map((categoryItem) => (
+            <div
+              key={categoryItem.category}
+              className="mb-12 bg-slate-900 p-4 rounded-2xl"
+            >
+              <h2 className="text-2xl font-bold text-white capitalize mb-6">
+                {categoryItem.category}
+              </h2>
+              <div className="grid gap-3 grid-cols-2 md:grid-cols-2">
+                {categoryItem.skills.map((skill) => (
+                  <SkillCard key={skill.id} {...skill} />
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
